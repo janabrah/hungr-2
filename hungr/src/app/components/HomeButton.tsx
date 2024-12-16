a34db3ab9624d9288df788bb5e11d15a01a9ab4a
Jacob Abrahams <cobynonsense@gmail.com>
2024-12-16 15:26:43 -0800

    Quick color change

--- hungr/src/app/components/HomeButton.tsx
+++ hungr/src/app/components/HomeButton.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const HomeButton = () => {
   return (
     <Link href="/" passHref>
-      <div className="inline-block bg-gray-800 text-white py-1 px-2 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 text-sm cursor-pointer">
+      <div className="inline-block bg-black-800 text-white py-1 px-2 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 text-sm cursor-pointer">
         <Image src="/icon.png" alt="Home" width={46} height={46} />
       </div>
     </Link>
