3e88e476fa65721354d7553bba40c5cf65f74c82
Coby Abrahams <coby@meter.com>
2026-01-04 14:56:12 -0600

    Setting up vite and tightening up typescript

--- hungr/src/app/layout.tsx
+++ hungr/src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import HomeButton from "./components/HomeButton";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import HomeButton from './components/HomeButton';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Hungr",
-  description: "Store and share your recipes here.",
+  title: 'Hungr',
+  description: 'Store and share your recipes here.',
 };
 
 export default function RootLayout({
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
